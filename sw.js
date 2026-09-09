199bd99859f4d310f5df6930a7098b2428afe1ae
Mogrogu <132185517+Mogrogu@users.noreply.github.com>
2026-09-08 20:41:06 -0400

    Add service worker for caching resources

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = 'archicalc-cache-v1.3';
+const urlsToCache = [
+  'index.html',
+  'CalcIcon.png',
+  'manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
